1b166fe14eb3e81b7f4ef32f28c94afcf0a96ebb
Joseph Congdon <jocongdon@yahoo.com>
2021-10-24 00:08:21 -0500

    finished codewars java lesson 2

--- src/main/Rental.java
+++ src/main/Rental.java
@@ -3,7 +3,17 @@ package main;
 public class Rental {
 
     public static int rentalCarCost(int d){
+    int totalRent = d * 40;
+    if(d >= 7){
+    return totalRent - 50;
+    }else if(d >= 3 && d <= 6){
+        return totalRent - 20;
+        }
+        return totalRent;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(rentalCarCost(7));
     }
 
 }
