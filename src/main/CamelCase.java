78c2074b15a6cb8067671c54c65d4a707e3273d1
Joseph Congdon <jocongdon@yahoo.com>
2021-12-18 15:40:50 -0600

    working solution

--- src/main/CamelCase.java
+++ src/main/CamelCase.java
@@ -9,20 +9,43 @@ package main;
 
 // So we need to go through the string look at each character. the following character to a _ or a - needs to be set .toUpperCase
 
+import java.util.Locale;
+
 public class CamelCase {
 
-    static String toCamelCase(String s){
+//    public static void toCamelCase(String s){
+//        for(int i = 0; i >= s.length(); i++){
+//            char currentChar = s.charAt(i);
+//            String nextChar = String.valueOf(s.charAt(i + 1));
+//            if(currentChar == '-' || currentChar == '_'){
+//                System.out.println(nextChar.toUpperCase());
+//            }
+//        }
+//        return;
+//    }
 
-        for(int i = 0; i >= s.length(); i++){
-            char currentChar = s.charAt(i);
-            if(currentChar == '-' || currentChar == '_'){
+    public class ConvertStringToCamelCase {
+        public static String toCamelCase(String s) {
+            final String regex = "([^a-zA-Z']+)'*\\1*";
+            final String[] split = s.split(regex);
+            final StringBuilder sb = new StringBuilder();
 
+            for (int i = 0; i < split.length; i++) {
+                if (i == 0) {
+                    sb.append(split[i]);
+                } else if (split[i].length()>0){
+                    sb.append(split[i].replaceFirst(split[i].substring(0, 1), split[i].substring(0, 1).toUpperCase()));
+                }
             }
+            return new String(sb);
         }
-
-        return "";
     }
 
 
+
+
+    public static void main(String[] args) {
+        System.out.println(ConvertStringToCamelCase.toCamelCase("john_lennon"));
+    }
 }
 
