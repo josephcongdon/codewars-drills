cc67daa12eee1ba0508db1e7995cd046d9290072
Joseph Congdon <jocongdon@yahoo.com>
2021-12-29 21:30:39 -0600

    started basic arrays

--- src/main/ArraysExercises.java
+++ src/main/ArraysExercises.java
@@ -1,14 +1,38 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class ArraysExercises {
 
-
     public static void main(String[] args) {
-    int[] num = {1,2,3,4,5};
-        String number = Arrays.toString(num);
-        System.out.println(number);
+//    int[] num = {1,2,3,4,5};
+//        String number = Arrays.toString(num);
+//        System.out.println(number);
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+        Person[] person = {
+         new Person ("Sarah"),
+         new Person ("Ben"),
+         new Person ("Bekah"),
+         new Person ("Joe"),
+         new Person ("Abby"),
+         new Person ("Lydia"),
+         new Person ("Caleb"),
+         new Person ("Josh")
+        };
+            for(Person p : person){
+                System.out.println(p.getName());
+            }
+        }
+
+
     }
 
-}
+
