a0a6e69be1febb9a5f5452835ab9691bc6d8041f
Joseph Congdon <jocongdon@yahoo.com>
2021-12-30 12:09:41 -0600

    addede a do while to the add person function

--- src/main/ArraysExercises.java
+++ src/main/ArraysExercises.java
@@ -1,9 +1,6 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ArraysExercises {
@@ -12,27 +9,30 @@ public class ArraysExercises {
 //    int[] num = {1,2,3,4,5};
 //        String number = Arrays.toString(num);
 //        System.out.println(number);
+        String input;
+        do {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.printf("please add a new name to the list: ");
-        String input = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            System.out.printf("please add a new name to the list: ");
+            input = scanner.nextLine();
 
-        List<Person> people = new ArrayList<Person>();
-             people.add(new Person("Sarah"));
-             people.add(new Person("Ben"));
-             people.add(new Person("Bekah"));
-             people.add(new Person("Joe"));
-             people.add(new Person("Abby"));
-             people.add(new Person("Lydia"));
-             people.add(new Person("Caleb"));
-             people.add(new Person("Josh"));
+            List<Person> people = new ArrayList<Person>();
+            people.add(new Person("Sarah"));
+            people.add(new Person("Ben"));
+            people.add(new Person("Bekah"));
+            people.add(new Person("Joe"));
+            people.add(new Person("Abby"));
+            people.add(new Person("Lydia"));
+            people.add(new Person("Caleb"));
+            people.add(new Person("Josh"));
 
             Person p = new Person(input);
             people.add(p);
-           for (Person i : people)
-        System.out.println(i.getName());
+            for (Person i : people)
+                System.out.println(i.getName());
+        } while (!input.equals("Bob"));
 
-        }
+    }
 
 
     }
