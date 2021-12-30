0e73f5326065cf2bbdd040c7f5d6603d19ad4f82
Joseph Congdon <jocongdon@yahoo.com>
2021-12-29 20:38:39 -0600

    square sum solution

---
+++ src/main/ArraysExercises.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.Arrays;
+
+public class ArraysExercises {
+
+
+    public static void main(String[] args) {
+    int[] num = {1,2,3,4,5};
+        String number = Arrays.toString(num);
+        System.out.println(number);
+    }
+
+}
---
+++ src/main/SquareSum.java
@@ -0,0 +1,20 @@
+package main;
+
+public class SquareSum {
+
+    public static int squareSum(int[] numbers) {
+        int sum = 0;
+        for (int n : numbers) {
+            sum += n * n;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+
