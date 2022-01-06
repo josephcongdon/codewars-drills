4138e4dc83435b66642a17b7587466aa8f9df4c2
Joseph Congdon <jocongdon@yahoo.com>
2022-01-05 18:06:50 -0600

    need to separate binary into individual numbers

--- src/js/countBits.js
+++ src/js/countBits.js
@@ -1,6 +1,17 @@
 var countBits = function(num) {
     // Program Me
+    let newTotal = 0;
+    let newBinary = "";
     let binary = (num % 2).toString();
-    binary.split(" ");
-
+    for (; num > 1; ) {
+        num = parseInt(num/2);
+        binary =  (num % 2) + (binary);
+        newBinary += binary.split(',');
+        for(let i = 0; i <= newBinary.length; i++){
+            if (i === 1){
+                newTotal += i;
+            }
+        }
+    }
+return newTotal;
 };
